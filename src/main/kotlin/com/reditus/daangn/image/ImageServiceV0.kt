04ffdd7cc4ab3bab8d40d4321ccab15e6730eb71
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-02-24 14:12:47 +0900

    [Fix]: ImageService 로직 오류 수정

--- src/main/kotlin/com/reditus/daangn/image/ImageServiceV0.kt
+++ src/main/kotlin/com/reditus/daangn/image/ImageServiceV0.kt
@@ -36,8 +36,7 @@ class ImageServiceV0 : ImageService {
 
         return userFile?.filter {
             val name = it.nameWithoutExtension
-            val fileId = name.substringAfter("-")
-            imageIds.contains(fileId)
+            imageIds.contains(name)
         }?.toList() ?: emptyList()
     }
 
@@ -59,8 +58,7 @@ class ImageServiceV0 : ImageService {
         val userFile = folder.listFiles() ?: throw IllegalArgumentException("이미지가 존재하지 않습니다.")
         val files = userFile.filter {
             val name = it.nameWithoutExtension
-            val fileId = name.substringAfter("-")
-            imageIds.contains(fileId)
+            imageIds.contains(name)
         }
         if(files.size != imageIds.size){
             throw IllegalArgumentException("이미지가 존재하지 않습니다.")
