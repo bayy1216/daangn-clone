5b30e16e87e5a5c56f1a3533b7a87c6ac3615468
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-02-25 01:22:27 +0900

    [Feat]: 스웨거 config에 jwt 추가

--- src/main/kotlin/com/reditus/daangn/core/controller/interceptor/annotation/Login.kt
+++ src/main/kotlin/com/reditus/daangn/core/controller/interceptor/annotation/Login.kt
@@ -1,9 +1,12 @@
 package com.reditus.daangn.core.controller.interceptor.annotation
 
+import io.swagger.v3.oas.annotations.Hidden
+
 /**
  * 로그인이 필요한 API에 사용하는 어노테이션
  * [UserAuth]와 함께사용
  */
+@Hidden // swagger 문서에 표시하지 않음
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.VALUE_PARAMETER)
 annotation class Login()
--- src/main/kotlin/com/reditus/daangn/core/controller/swagger/SwaggerConfig.kt
+++ src/main/kotlin/com/reditus/daangn/core/controller/swagger/SwaggerConfig.kt
@@ -3,6 +3,7 @@ package com.reditus.daangn.core.controller.swagger
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -11,7 +12,22 @@ import org.springframework.context.annotation.Configuration
 class SwaggerConfig {
     @Bean
     fun openAPI(): OpenAPI = OpenAPI()
+        .components(components())
         .info(apiInfo())
+
+    private fun components() = Components()
+        .addSecuritySchemes("bearer-key", bearerJwtSecurityScheme())
+
+
+    /**
+     * Bearer JWT Security Scheme
+     * 컨트롤러에서 사용하려면 @SecurityRequirement(name = "bearer-key") 를 사용해야함
+     */
+    private fun bearerJwtSecurityScheme() = SecurityScheme()
+        .type(SecurityScheme.Type.HTTP)
+        .scheme("bearer")
+        .bearerFormat("JWT")
+        .description("JWT 인증을 위한 토큰을 입력하세요. (예: Bearer {token}에서 token만 입력)")
 
     private fun apiInfo() = Info()
         .title("당근마켓 클론코딩 API")
